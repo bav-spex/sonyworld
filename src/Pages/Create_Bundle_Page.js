6e11b9e01411e9d2b993e734e0d341422d7dd7a0
bbavishie <bbavishi@sigmasolve.net>
2022-05-07 08:21:05 +0530

    product_details_page_complete_design_small_ui_change

--- src/Pages/Create_Bundle_Page.js
+++ src/Pages/Create_Bundle_Page.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import BreadCrumbs from '../Components/BreadCrumbs'
+import TopNavbar from '../Components/TopNavbar'
 
 function Create_Bundle_Page() {
   return (
-    <div>Create_Bundle_Page</div>
+    <>
+    <TopNavbar />
+    <BreadCrumbs/>
+    <div className="container-fluid create__bundle__page__container">
+      <div className="row create__bundle__page__block">
+        <div className="col-md-12 col-xl-9 row create__bundle__left__block">
+          <div className="row cb__product__block">
+            <div className="col-12 col-sm-3 cb__product__left__block"></div>
+            <div className="col-12 col-sm-9 cb__product__right__block"></div>
+          </div>
+        </div>
+        <div className="col-md-12 col-xl-9  create__bundle__right__block"></div>
+      </div>
+    </div>
+    </>
   )
 }
 
