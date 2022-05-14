3b516a5732d0d75c3ea50fead7ce4bbad6e4476e
Bhavik Bavishi <bbavishi@sigmasolve.net>
2022-05-14 11:09:59 +0000

    Dashboard my orders order details

--- src/Components/Font/Heading1.js
+++ src/Components/Font/Heading1.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./../../SCSS/Font/_font.scss";
-function Heading1({ text, color, marginBottom, marginLeft,span }) {
+function Heading1({ text, color, marginBottom, marginLeft, span,textAlign }) {
   return (
     <>
       {!span ? (
         <p
           style={{
+             textAlign:`${textAlign}`,
             color: `${color}`,
-            margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${marginLeft ? marginLeft : 0}px`,
+            margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${
+              marginLeft ? marginLeft : 0
+            }px`,
           }}
           className="heading1"
         >
@@ -17,15 +20,17 @@ function Heading1({ text, color, marginBottom, marginLeft,span }) {
       ) : (
         <span
           style={{
+             textAlign:`${textAlign}`,
             color: `${color}`,
-            margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${marginLeft ? marginLeft : 0}px`,
+            margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${
+              marginLeft ? marginLeft : 0
+            }px`,
           }}
           className="heading1"
         >
           {text}
         </span>
       )}
-     
     </>
   );
 }
--- src/Components/Font/Text5.js
+++ src/Components/Font/Text5.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./../../SCSS/Font/_font.scss";
-function Text5({ text, color, marginBottom,marginLeft, lineThrough,span  }) {
+function Text5({ text, color, marginBottom,marginLeft, lineThrough,span  ,textAlign}) {
   return (
     <>
       {!span ? (
         <p
           style={{
+             textAlign:`${textAlign}`,
             color: `${color}`,
             textDecoration: `${lineThrough ? "line-through" : "none"}`,
             margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${marginLeft ? marginLeft : 0}px`,
@@ -17,6 +18,7 @@ function Text5({ text, color, marginBottom,marginLeft, lineThrough,span  }) {
       ) : (
         <span
           style={{
+             textAlign:`${textAlign}`,
             color: `${color}`,
             textDecoration: `${lineThrough ? "line-through" : "none"}`,
             margin: `0px 0px ${marginBottom ? marginBottom : 0}px ${marginLeft ? marginLeft : 0}px`,
